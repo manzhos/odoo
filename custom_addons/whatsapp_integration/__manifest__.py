4119ca883bfbaa21a2d517d3a5dce4bac52a0f74
Vladimir Manzhos <manzhos@gmail.com>
2024-12-15 14:19:47 +0100

    modified:   custom_addons/whatsapp_integration/__manifest__.py

--- custom_addons/whatsapp_integration/__manifest__.py
+++ custom_addons/whatsapp_integration/__manifest__.py
@@ -5,10 +5,10 @@
   "author": "vladimir manzhos",
   "website": "manzhos.cz",
   "version": "18.0.1.0",
-  "depends": [
-    'base',
-    'mail'
-  ],
+  # "depends": [
+  #   'base',
+  #   'mail'
+  # ],
   "data" : [
     "views/whatsapp_views.xml"
   ],
