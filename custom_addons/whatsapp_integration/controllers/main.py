6368202ce8f042cf72d430b2c25d4c0766ef4e90
Vladimir Manzhos <manzhos@gmail.com>
2024-12-15 16:02:36 +0100

    modified:   custom_addons/whatsapp_integration/controllers/main.py 	modified:   custom_addons/whatsapp_integration/models/whatsapp_int.py

--- custom_addons/whatsapp_integration/controllers/main.py
+++ custom_addons/whatsapp_integration/controllers/main.py
@@ -4,12 +4,12 @@ from odoo.http import request
 class WhatsappController(http.Controller):
     @http.route('/whatsapp/webhook', type='json', auth='public', methods=['POST'])
     def handle_incoming_message(self, **kwargs):
-        message = kwargs.get('message')
-        phone_number = kwargs.get('phone_number')
+    #     message = kwargs.get('message')
+    #     phone_number = kwargs.get('phone_number')
         
-        # Process the incoming message here, e.g., create a log, send an automated reply, etc.
-        request.env['whatsapp.integration'].create({
-            'message': message,
-            'phone_number': phone_number,
-        })
+    #     # Process the incoming message here, e.g., create a log, send an automated reply, etc.
+    #     request.env['whatsapp.integration'].create({
+    #         'message': message,
+    #         'phone_number': phone_number,
+    #     })
         return {'status': 'ok'}
